9bbf7d234acf8d94f91794ae726cbfa5b9a92326
Maxim Kalaev <maximk@il.ibm.com>
2013-04-26 12:30:46 +0300

    Adding tests

---
+++ tests/test_depstrace.py
@@ -0,0 +1,148 @@
+#!/usr/bin/python
+#
+# Copyright 2013 Maxim Kalaev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import depstrace
+
+class DepsTraceTests(unittest.TestCase):
+    def testDepStraceLogIterator(self):
+        tracefile = """5082  execve("/bin/ninja", [...], [/* 45 vars */]) = 0
+5082  chdir("/home/build/") = 0
+5082  rename("version_info/contained_commits~", "version_info/contained_commits") = 0
+5082  open("tst.c", O_RDONLY) = 5
+5082  open("tst.o", O_WRONLY|O_CREAT|O_TRUNC|O_LARGEFILE) = 6
+5082  symlink(".build/install", "install") = 0
+5086  execve("gcc", [...], [/* 45 vars */] <unfinished ...>
+5085  open("tst.h", O_RDONLY|O_NOCTTY|O_LARGEFILE <unfinished ...>
+5082  clone(child_stack=0, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0xb77c8768) = 5555
+5082  clone( <unfinished ...>
+4294  vfork( <unfinished ...>
+5086  <... execve resumed> ) = 0
+5085  <... open resumed> ) = 7
+4294  <... vfork resumed> ) = 4295
+5082  <... clone resumed> child_stack=0, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0x7f1483d089f0) = ? ERESTARTNOINTR (To be restarted)
+4120  wait4(-1,  <unfinished ...>
+4120  <... wait4 resumed> [{WIFEXITED(s) && WEXITSTATUS(s) == 0}], WNOHANG, NULL) = 4180
+4181  stat("/bin/ls", {st_mode=S_IFREG|0755, st_size=97736, ...}) = 0
+5085  exit_group(0) = ?
+"""
+        tracer = depstrace.DepsTracer()
+        tokens = list(tracer._strace_log_iter(tracefile.splitlines()))
+        self.assertItemsEqual(tracer.unmatched_lines, [])
+
+        # execve
+        self.assertEqual(tokens.pop(0), ('5082', 'execve', '0', '/bin/ninja', '[...]'))
+        # chdir
+        self.assertEqual(tokens.pop(0), ('5082', 'chdir', '0', '/home/build/', None))
+        # rename
+        self.assertEqual(tokens.pop(0), ('5082', 'rename', '0', 'version_info/contained_commits~',
+                                         'version_info/contained_commits'))
+        # open one flag
+        self.assertEqual(tokens.pop(0), ('5082', 'open', '5', 'tst.c', 'O_RDONLY'))
+        # open flags OR-ed
+        self.assertEqual(tokens.pop(0), ('5082', 'open', '6', 'tst.o', 'O_WRONLY|O_CREAT|O_TRUNC|O_LARGEFILE'))
+        # symlink
+        self.assertEqual(tokens.pop(0), ('5082', 'symlink', '0', '.build/install', 'install'))
+        # clone
+        self.assertEqual(tokens.pop(0), ('5082', 'clone', '5555', 'child_stack=0',
+                                         'flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD'))
+        # execve unfinished and then resumed
+        self.assertEqual(tokens.pop(0), ('5086', 'execve', '0', 'gcc', '[...]'))
+        # open unfinished and then resumed
+        self.assertEqual(tokens.pop(0), ('5085', 'open', '7', 'tst.h', 'O_RDONLY|O_NOCTTY|O_LARGEFILE'))
+        # vfork unfinished and then resumed
+        self.assertEqual(tokens.pop(0), ('4294', 'vfork', '4295', None, None))
+        # clone non-return (ERESTARTNOINTR)
+        self.assertEqual(tokens.pop(0), ('5082', 'clone', '?', 'child_stack=0',
+                                         'flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD'))
+
+        # Complex parameter expression
+        self.assertEqual(tokens.pop(0), ('4120', 'wait4', '4180', '-1', '[{WIFEXITED(s) && WEXITSTATUS(s) == 0}]'))
+
+        # Another complex parameter expression
+        self.assertEqual(tokens.pop(0), ('4181', 'stat', '0', '/bin/ls', '{st_mode=S_IFREG|0755, st_size=97736, ...}'))
+
+        # non-return
+        self.assertEqual(tokens.pop(0), ('5085', 'exit_group', '?', '0', None))
+
+        # Ensure all the tockens were fetched
+        self.assertEqual(0, len(tokens))
+
+    def testDepStraceTrackTwoRules(self):
+        tracefile = """5082  execve("/home/maximk/programs/bin/ninja", [...], [/* 45 vars */]) = 0
+5082  open("tst.d", O_RDONLY) = 5
+5082  open("tst2.d", O_RDONLY) = 5
+5082  clone(child_stack=0, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0xb77c8768) = 5085
+5082  clone(child_stack=0, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0xb77c8768) = 5086
+5085  execve("/usr/bin/gcc", [...], [/* 45 vars */] <unfinished ...>
+5086  execve("gcc", [...], [/* 45 vars */] <unfinished ...>
+5085  <... execve resumed> ) = 0
+5086  <... execve resumed> ) = 0
+5085  open("tst.c", O_RDONLY|O_NOCTTY|O_LARGEFILE <unfinished ...>
+5086  open("tst.c", O_RDONLY|O_NOCTTY|O_LARGEFILE <unfinished ...>
+5085  <... open resumed> ) = 4
+5086  <... open resumed> ) = 4
+5085  open("tst.h", O_RDONLY|O_NOCTTY|O_LARGEFILE <unfinished ...>
+5086  open("tst.h", O_RDONLY|O_NOCTTY|O_LARGEFILE <unfinished ...>
+5085  <... open resumed> ) = 5
+5086  <... open resumed> ) = 5
+5085  open("tst2.d", O_WRONLY|O_CREAT|O_TRUNC|O_LARGEFILE, 0666) = 4
+5086  open("tst.d", O_WRONLY|O_CREAT|O_TRUNC|O_LARGEFILE, 0666) = 4
+5086  open("tst", O_RDWR|O_CREAT|O_TRUNC|O_LARGEFILE, 0666 <unfinished ...>
+5086  <... open resumed> ) = 5
+5085  open("tst2", O_RDWR|O_CREAT|O_TRUNC|O_LARGEFILE, 0666) = 4
+5086  open("tst", O_RDWR|O_LARGEFILE) = 13
+5085  open("tst2", O_RDWR|O_LARGEFILE) = 13
+"""
+        tracer = depstrace.DepsTracer(None)
+        rules = tracer.parse_trace(tracefile.splitlines())
+        self.assertEqual(2, len(rules))
+
+        r = rules.pop(0)
+        self.assertItemsEqual(r.deps, ['tst.c', 'tst.h'])
+        self.assertItemsEqual(r.outputs, ['tst2', 'tst2.d'])
+        self.assertEqual(r.pid, '5085')
+        self.assertEqual(r.lineno, 4)
+
+        r = rules.pop(0)
+        self.assertItemsEqual(r.deps, ['tst.c', 'tst.h', 'gcc'])
+        self.assertItemsEqual(r.outputs, ['tst', 'tst.d'])
+        self.assertEqual(r.pid, '5086')
+        self.assertEqual(r.lineno, 5)
+
+        self.assertItemsEqual(tracer.unmatched_lines, [])
+
+    def testDepStraceUnmatched(self):
+        tracefile = """5082  execve("/home/maximk/programs/bin/ninja", [...], [/* 45 vars */]) = 0
+5082  clone(child_stack=0, flags=CLONE_CHILD_CLEARTID|CLONE_CHILD_SETTID|SIGCHLD, child_tidptr=0xb77c8768) = 5085
+5085  execve("tst.sh", [...], [/* 45 vars */]) = 0
+5085  open("tst", O_RDWR) = 4
+5085  unmatchedop("tst.c") = 5
+"""
+        tracer = depstrace.DepsTracer()
+        rules = tracer.parse_trace(tracefile.splitlines())
+        self.assertEqual(1, len(rules))
+
+        r = rules.pop(0)
+        self.assertItemsEqual(r.deps, ['tst.sh'])
+        self.assertItemsEqual(r.outputs, ['tst'])
+        self.assertEqual(r.pid, '5085')
+        self.assertEqual(r.lineno, 2)
+
+        self.assertItemsEqual(tracer.unmatched_lines, ['5085  unmatchedop("tst.c") = 5'])
+
+    def testDepStraceTrackChdir(self):
+        tracefile = """"""
---
+++ tests/test_parsers.py
@@ -0,0 +1,346 @@
+#!/usr/bin/python
+#
+# Copyright 2013 Maxim Kalaev
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+import depslint
+
+# Silence all output
+depslint._verbose = -1
+
+class NinjaManifestParserIOHooked(depslint.NinjaManifestParser):
+    def __init__(self, manifest, files_dict={}):
+        self.files = files_dict
+        super(NinjaManifestParserIOHooked, self).__init__(manifest)
+
+    def _read_depfile(self, path):
+        return self.files.get(path)
+
+class NinjaManifestParserTests(unittest.TestCase):
+    def testBuildRulesParse(self):
+        manifest = """
+rule RULE
+  command = cc $in -o $out
+
+build out1 out2: RULE in1 in2 | ein1 ein2 || oin1 oin2
+
+build out3: RULE in2 | in1
+build out4: RULE || oin3
+build out5: RULE | in4
+build out6: RULE in5
+build out7: RULE | || in6
+build out8: RULE | ||
+build out9: RULE ||
+build outA: RULE |
+"""
+        parser = NinjaManifestParserIOHooked(manifest.splitlines())
+        edges = list(parser.iterate_target_rules())
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out1", "out2"])
+        self.assertItemsEqual(e.deps, ["in1", "in2", "ein1", "ein2"])
+        self.assertItemsEqual(e.order_only_deps, ["oin1", "oin2"])
+        self.assertEqual(e.rule, "RULE")
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "in1 in2")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out3"])
+        self.assertItemsEqual(e.deps, ["in1", "in2"])
+        self.assertItemsEqual(e.order_only_deps, [])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "in2")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "out3")
+        self.assertEqual(e.rule, "RULE")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out4"])
+        self.assertItemsEqual(e.deps, [])
+        self.assertItemsEqual(e.order_only_deps, ["oin3"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "out4")
+        self.assertEqual(e.rule, "RULE")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out5"])
+        self.assertItemsEqual(e.deps, ["in4"])
+        self.assertItemsEqual(e.order_only_deps, [])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "out5")
+        self.assertEqual(e.rule, "RULE")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out6"])
+        self.assertItemsEqual(e.deps, ["in5"])
+        self.assertItemsEqual(e.order_only_deps, [])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "in5")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "out6")
+        self.assertEqual(e.rule, "RULE")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out7"])
+        self.assertItemsEqual(e.deps, [])
+        self.assertItemsEqual(e.order_only_deps, ["in6"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "out7")
+        self.assertEqual(e.rule, "RULE")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out8"])
+        self.assertItemsEqual(e.deps, [])
+        self.assertItemsEqual(e.order_only_deps, [])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "out8")
+        self.assertEqual(e.rule, "RULE")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out9"])
+        self.assertItemsEqual(e.deps, [])
+        self.assertItemsEqual(e.order_only_deps, [])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "out9")
+        self.assertEqual(e.rule, "RULE")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["outA"])
+        self.assertItemsEqual(e.deps, [])
+        self.assertItemsEqual(e.order_only_deps, [])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "outA")
+        self.assertEqual(e.rule, "RULE")
+
+        self.assertItemsEqual(edges, list())
+
+    def testManifestPathsNormalization(self):
+        manifest = """
+# Paths normalization
+build a/../outB: phony ./a/b | a//c || a/../a/d
+"""
+        parser = NinjaManifestParserIOHooked(manifest.splitlines())
+        edges = list(parser.iterate_target_rules())
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["outB"])
+        self.assertItemsEqual(e.deps, ["a/b", "a/c"])
+        self.assertItemsEqual(e.order_only_deps, ["a/d"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "a/b")
+        self.assertEqual(parser._eval_edge_attribute(e, "out"), "outB")
+        self.assertEqual(e.rule, "phony")
+
+        self.assertItemsEqual(edges, list())
+
+    def testAtributes(self):
+        manifest = """
+v1 = out
+v2 = in
+v3 = g
+
+rule RULE
+  command = cc $in -o $out
+  v3 = r
+
+# Using variables in build contructs, with and w/o {}
+# Build scope overrides definitions from global and rule scopes,
+# including build block filenames.
+build ${v1}: RULE $v2
+  v1 = out1
+  v2 = in1
+  v4 = $v1/$v2 $v3
+
+# Embedding varibles in a string
+build ${v1}2: RULE prefix$v2.ext
+
+# Rule scope and global scope
+build ${v1}3: phony
+"""
+        parser = NinjaManifestParserIOHooked(manifest.splitlines())
+        edges = list(parser.iterate_target_rules())
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out1"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "in1")
+        self.assertEqual(parser._eval_edge_attribute(e, "v1"), "out1")
+        self.assertEqual(parser._eval_edge_attribute(e, "v2"), "in1")
+        self.assertEqual(parser._eval_edge_attribute(e, "v3"), "r")
+        self.assertEqual(parser._eval_edge_attribute(e, "v4"), "out1/in1 r")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out2"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "prefixin.ext")
+        self.assertEqual(parser._eval_edge_attribute(e, "v1"), "out")
+        self.assertEqual(parser._eval_edge_attribute(e, "v2"), "in")
+        self.assertEqual(parser._eval_edge_attribute(e, "v3"), "r")
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out3"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "v1"), "out")
+        self.assertEqual(parser._eval_edge_attribute(e, "v2"), "in")
+        self.assertEqual(parser._eval_edge_attribute(e, "v3"), "g")
+
+        self.assertItemsEqual(edges, list())
+
+    @unittest.skip("Fixme later")
+    def testEscapes(self):
+        manifest = """
+v2 = in
+v3 = g
+
+# Escaping variables
+build $${v1}4: phony $$v2
+  v3 = $$v3
+"""
+        parser = NinjaManifestParserIOHooked(manifest.splitlines())
+        edges = list(parser.iterate_target_rules())
+
+        # Fix needed!
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["${v1}4"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "$v2")
+        self.assertEqual(parser._eval_edge_attribute(e, "v1"), "out")
+        self.assertEqual(parser._eval_edge_attribute(e, "v2"), "in")
+        self.assertEqual(parser._eval_edge_attribute(e, "v3"), "$v3")
+
+        self.assertItemsEqual(edges, list())
+
+    @unittest.skip("Fixme later")
+    def testManifestRecursiveAttrs(self):
+        manifest = """
+# Recursive attribute redefinition
+v2 = in
+
+build out: phony $v2
+  v2 = $v2.$v2
+"""
+        parser = NinjaManifestParserIOHooked(manifest.splitlines())
+        edges = list(parser.iterate_target_rules())
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "in.in")
+        self.assertEqual(parser._eval_edge_attribute(e, "v2"), "in.in")
+
+        self.assertItemsEqual(edges, list())
+
+    def testDepfilesLoading(self):
+        manifest = """
+rule RULE
+  depfile = $out.d
+
+build out1: RULE in1 | in2 || oin3
+
+# Empty depfile
+build out2: RULE || oin3
+
+# Missing depfile
+build out3: RULE
+"""
+        files = ({"out1.d":"out1: din1 din2",
+                  "out2.d":"out2: "})
+        parser = NinjaManifestParserIOHooked(manifest.splitlines(), files)
+        edges = list(parser.iterate_target_rules())
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out1"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "in1")
+        self.assertEqual(parser._eval_edge_attribute(e, "depfile"), "out1.d")
+        self.assertItemsEqual(e.deps, ["in1", "in2"])
+        self.assertItemsEqual(e.depfile_deps, ["din1", "din2"])
+        self.assertItemsEqual(e.order_only_deps, ["oin3"])
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out2"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "depfile"), "out2.d")
+        self.assertItemsEqual(e.deps, [])
+        self.assertItemsEqual(e.depfile_deps, [])
+        self.assertItemsEqual(e.order_only_deps, ["oin3"])
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets,["out3"])
+        self.assertEqual(parser._eval_edge_attribute(e, "in"), "")
+        self.assertEqual(parser._eval_edge_attribute(e, "depfile"), "out3.d")
+        self.assertItemsEqual(e.deps, [])
+        self.assertItemsEqual(e.depfile_deps, [])
+        self.assertItemsEqual(e.order_only_deps, [])
+
+        self.assertItemsEqual(edges, list())
+
+class DepfilesParsingTests(unittest.TestCase):
+    def testDepfilesTypicalParse(self):
+        depfile = """out1:"""
+        parser = depslint.DepfileParser()
+        targets, deps = parser.parse_depfile(depfile)
+        self.assertItemsEqual(targets, ["out1"])
+        self.assertItemsEqual(deps, [])
+
+        depfile = """out: in1 in2"""
+        targets, deps = parser.parse_depfile(depfile)
+        self.assertItemsEqual(targets, ["out"])
+        self.assertItemsEqual(deps, ["in1", "in2"])
+
+        depfile = r"""out: \
+in1 \
+in2"""
+        targets, deps = parser.parse_depfile(depfile)
+        self.assertItemsEqual(targets, ["out"])
+        self.assertItemsEqual(deps, ["in1", "in2"])
+
+    def testDepfilesWhitespacesAndSpecials(self):
+        depfile = r"""
+out\ 1: in\ 1.h in\ 2.h \
+in\3.h in\\4.h c:\ms(x86)\h @conf+-=.h"""
+        parser = depslint.DepfileParser()
+        targets, deps = parser.parse_depfile(depfile)
+        self.assertItemsEqual(targets, ["out 1"])
+        self.assertItemsEqual(deps, ["in 1.h", "in 2.h", r"in\3.h", r"in\4.h",
+                                     r"c:\ms(x86)\h",
+                                     "@conf+-=.h"])
+
+    def testDepfilesMultitargets(self):
+        depfile = r"""out\ 1 out\ 2: in\ 1.h in\ 2.h"""
+        parser = depslint.DepfileParser()
+        targets, deps = parser.parse_depfile(depfile)
+        self.assertItemsEqual(targets, ["out 1", "out 2"])
+        self.assertItemsEqual(deps, ["in 1.h", "in 2.h"])
+
+    def testDepfilesPathsNormalization(self):
+        depfile = r"""out//out1 ./out2: ./../in1.h /a/../in2.h"""
+        parser = depslint.DepfileParser()
+        targets, deps = parser.parse_depfile(depfile)
+        self.assertItemsEqual(targets, ["out/out1", "out2"])
+        self.assertItemsEqual(deps, ["../in1.h", "/in2.h"])
+
+
+class TraceParserTests(unittest.TestCase):
+    def testIterateTargetRules(self):
+        input = """{'OUT': ['out1', 'out2'], 'IN': ['in1', 'in2']}
+{'OUT': ['out3'], 'IN': []}
+""".splitlines()
+        parser = depslint.TraceParser(input)
+        parser.iterate_target_rules()
+        edges = list(parser.iterate_target_rules())
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets, ['out1', 'out2'])
+        self.assertItemsEqual(e.deps, ['in1', 'in2'])
+        self.assertItemsEqual(e.depfile_deps, [])
+        self.assertItemsEqual(e.order_only_deps, [])
+
+        e = edges.pop(0)
+        self.assertItemsEqual(e.targets, ['out3'])
+        self.assertItemsEqual(e.deps, [])
+        self.assertItemsEqual(e.depfile_deps, [])
+        self.assertItemsEqual(e.order_only_deps, [])
+
+        self.assertItemsEqual(edges, list())
